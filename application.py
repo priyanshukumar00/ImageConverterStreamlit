104bfe9d5619fa8e37ae7f4b3ddad82086f43d45
Priyanshu <98214440+priyanshukumar00@users.noreply.github.com>
2023-03-21 13:23:50 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,26 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  8 13:21:53 2023
-
-@author: 91999
-"""
-
 import streamlit as st
 from PIL import Image
 
-def greyscale(image):
-    return image.convert('L')
+def rgb_to_gray(image):
+    gray = image.convert('L')
+    return gray
 
-st.title("Greyscale Image Converter")
+st.title('RGB to Greyscale Image Converter')
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
-    image = Image.open("image.jpg")
-    st.image(image, caption='Original Image', use_column_width=True)
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Original RGB Image', use_column_width=True)
+
     if st.button('Convert to Greyscale'):
-        greyscale_image = greyscale(image)
-        st.image(greyscale_image, caption='Greyscale Image', use_column_width=True)
-
-
+        gray_image = rgb_to_gray(image)
+        st.image(gray_image, caption='Converted Greyscale Image', use_column_width=True)
 
